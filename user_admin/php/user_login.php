7fbcde2a83ff0633d21625c8e6fbc812cf47f384
Shao Fei <sfatchs@yahoo.com.sg>
2015-06-10 10:15:39 +0800

    added user log in and user admin index page

---
+++ user_admin/php/user_login.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shao Fei
+ * Date: 9/6/2015
+ * Time: 6:29 PM
+ */
+
+session_start();
+
+$error_msg = "";
+
+if(!empty($_POST["username"]) && !empty($_POST["password"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $response = make_sql_query($username, $password);
+
+    if(mysqli_num_rows($response) == 1) {
+        $_SESSION["login_user"] = $username;
+        header("Location: ../user_admin_index.php");
+    } else {
+        header("Location: user_wrong_password.html");
+    }
+} else {
+    $error_msg = "Empty field(s)";
+}
+
+function make_sql_query($username, $password) {
+    require_once 'DB_connect/db_connect.php';
+    $connector = new DB_CONNECT();
+    $connector->connect();
+
+    $passwordHashed = md5($password);
+    $query = "SELECT * FROM User WHERE username='$username' AND password='$passwordHashed'";
+
+    $response = mysqli_query($connector->conn, $query);
+    $connector->close();
+    return $response;
+}
+
+?>
---
+++ user_admin/user_admin_index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <link rel="stylesheet" type="text/css" href="style/user_resgistration_style.css">
+    <meta charset="UTF-8">
+    <title>User admin</title>
+</head>
+<body>
+
+<h1> Hello <?php session_start(); echo $_SESSION['login_user']; ?></h1>
+
+</body>
+</html>
