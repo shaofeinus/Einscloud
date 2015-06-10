18770cb16b55d7a08626f46191db425c869df519
suheti <suheti.lee@gmail.com>
2015-06-10 12:59:02 +0800

    Upon login, all basic user info is loaded into $_SESSION variable.

--- user_admin/php/user_login.php
+++ user_admin/php/user_login.php
@@ -15,10 +15,20 @@ if(!empty($_POST["username"]) && !empty($_POST["password"])) {
     $password = $_POST["password"];
     $response = make_sql_query($username, $password);
 
+    //Check whether the query successfully returned 1 row of user data
     if(mysqli_num_rows($response) == 1) {
+    	//Load the basic information into $_SESSION.
+    	$row = mysqli_fetch_assoc($response);
+    	$_SESSION["login_id"] =  $row ["id"];
         $_SESSION["login_user"] = $username;
+        $_SESSION['login_firstname'] =  $row ["firstname"];
+        $_SESSION['login_lastname'] = $row ["lastname"];
+        $_SESSION['login_email'] = $row ["email"];
+        $_SESSION['login_nric'] = $row ["nric"];
+        $_SESSION['login_phone_no'] = $row ["phone_no"];
         header("Location: ../user_admin_index.php");
     } else {
+    	//TODO create the faulty page with going back link.
         header("Location: user_wrong_password.html");
     }
 } else {
--- user_admin/user_admin_index.php
+++ user_admin/user_admin_index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-    <link rel="stylesheet" type="text/css" href="style/user_resgistration_style.css">
-    <meta charset="UTF-8">
-    <title>User admin</title>
+<link rel="stylesheet" type="text/css"
+	href="style/user_resgistration_style.css">
+<meta charset="UTF-8">
+<title>User Admin Console</title>
 </head>
 <body>
+	<h1> Hello <?php session_start(); echo $_SESSION['login_firstname'].' '.$_SESSION['login_lastname']; ?></h1>
 
-<h1> Hello <?php session_start(); echo $_SESSION['login_user']; ?></h1>
 
 </body>
 </html>
