731d0253ffcb2e5fe27c7e57bec2ff4aae8a1a4f
JarrickLiu <jarrickliu@gmail.com>
2015-06-16 10:59:41 +0800

    daily email php

--- user_admin/caregive_email.php
+++ user_admin/caregive_email.php
@@ -31,14 +31,10 @@ function emailToViewer()
     ';
 
     //echo $message;
-// Always set content-type when sending HTML email
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-// More headers
     $headers .= 'From: <einscloud@gmail.com>' . "\r\n";
-    //$headers .= 'Cc: myboss@example.com' . "\r\n";
-    //header("Location: http://reddit.com");
     $result = mail($to, $subject, $message, $headers);
 }
 ?>
---
+++ user_admin/email_daily.php
@@ -0,0 +1,37 @@
+<?php
+    require_once 'php/DB_connect/db_utility.php';
+    $viewer_result = make_query("select * from UnregisteredViewer where email is not null");
+
+    if(mysqli_num_rows($viewer_result) > 0){
+        while($row = mysqli_fetch_assoc($viewer_result)){
+            $to = $row['email'];
+            $rowUserID = $row['user_id'];
+            $user_result = make_query("select firstname, lastname from User where id = '$rowUserID'");
+            $user_result_row = mysqli_fetch_assoc($user_result);
+            $user_name = $user_result_row['firstname'] . ' ' . $user_result_row['lastname'];
+            $verificationCode = $row['verification_code'];
+            $subject = $user_name . ' still needs you to be his Einswatch viewer';
+            $message = '
+                <html>
+                <body>
+                Dear Sir/Mdm,<br>
+                <p>We have noticed that you have not verified to be ' . $user_name . '\'s' . ' viewer. ' . $user_name . ' has
+                recently purchased the Einswatch and has requested that you download the mobile app to be a personal caregiver.
+                Your one-time verification code is "' . $verificationCode . '". Follow this <a href= "http://einstech.com">link</a> to
+                download the app.</p><br>
+                Best Regards,<br>
+                The Einswatch Team
+                </body>
+                </html>
+            ';
+            //echo $message;
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            $headers .= 'From: <einscloud@gmail.com>' . "\r\n";
+            $result = mail($to, $subject, $message, $headers);
+        }
+    }
+    else {
+        echo 'No unregistered viewer with emails';
+    }
+?>
