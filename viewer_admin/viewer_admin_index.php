7981f31ac0da2b9da12637e7ec0c2175856af8b9
JarrickLiu <jarrickliu@gmail.com>
2015-06-17 11:22:17 +0800

    The very 1st bootstrap attempt

--- viewer_admin/viewer_admin_index.php
+++ viewer_admin/viewer_admin_index.php
@@ -2,29 +2,142 @@
 <?php session_start(); ?>
 <html>
 <head lang="en">
-    <link rel="stylesheet" type="text/css" href="style/viewer_admin_style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="style/viewer_admin_style.css"> -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Viewer Admin</title>
 </head>
 <body>
-<script src="script/viewer_verify_script.js"></script>
+    <script src="script/viewer_verify_script.js"></script>
+    <div class="container">
+        <div class="jumbotron">
+        <h1>Hello <?php echo $_SESSION['login_viewer'] . ','; ?></h1>
 
-<h1> Hello <?php echo $_SESSION['login_viewer'] . ','; ?></h1>
-<?php
+        </div>
+    </div>
+    <div class="container">
+         <div class="page-header">
+        <h2>You are currently viewing: </h2>
+        </div>
+    </div>
+    <?php
+    require_once 'php/DB_connect/db_utility.php';
     $username = $_SESSION['login_viewer'];
     $viewer_id = $_SESSION['viewer_id'];
     $viewer_phone = $_SESSION['viewer_phone'];
-    //echo "viewer id is " . $viewer_id;
 
-    echo "<h3> You are currently viewing: </h3>";
 
-    queryCaregive($viewer_id);
+    $caregiveQuery = "SELECT firstname, lastname, birthday, gender, nric, phone_no FROM User, Caregive WHERE rv_id = '$viewer_id' AND user_id = User.id";
+    $caregiveResponse = make_query($caregiveQuery);
 
-    echo "<br><br><h3> Unverified Users Requiring Your Confirmation to View</h3>";
+    if($caregiveResponse === FALSE) {
+        echo "response is erroneous";
+        die(mysql_error());
+    }
+    if(mysqli_num_rows($caregiveResponse) > 0) {
+        ?>
+        <div class="container">
+            <div class="row">
+        <?php
+        while ($row = mysqli_fetch_assoc($caregiveResponse)) {
 
-    queryUnregistered($viewer_phone);
+            ?>
+
+                    <div class="col-sm-4">
+                        <h3><?php echo $row["firstname"] . " " . $row["lastname"] ?></h3>
+                        <table>
+                            <tr>
+                                <th>Age: </th>
+                                <td><?php echo date_diff(date_create($row["birthday"]), date_create('today'))->y ?></td>
+                            </tr>
+                            <tr>
+                                <th>Gender: </th>
+                                <td><?php echo $row["gender"]?></td>
+                            </tr>
+                            <tr>
+                                <th>NRIC: </th>
+                                <td><?php echo $row["nric"]?></td>
+                            </tr>
+                            <tr>
+                                <th>Phone Number: </th>
+                                <td><?php echo ' ' . $row["phone_no"]?></td>
+                            </tr>
+                        </table>
+                    </div>
+
+            <?php
+        }
+    ?>
+    </div></div>
+    <?php
+    }
+    else{
+        echo "You are viewing 0 users";
+        }
 
     ?>
+    <div class="container">
+        <div class="page-header">
+            <h2>Unverified Users Requiring Your Confirmation to View </h2>
+        </div>
+    </div>
+    <?php
+    $unregisteredQuery = "select id, firstname, lastname, U.phone_no, gender, birthday from UnregisteredViewer, User U where '$viewer_phone' = UnregisteredViewer.phone_no AND user_id = U.id";
+
+    $unregisteredResponse = make_query($unregisteredQuery);
+
+    if(mysqli_num_rows($unregisteredResponse) > 0) {
+        ?>
+        <div class="container">
+            <div class="row">
+                <?php
+                while ($row = mysqli_fetch_assoc($unregisteredResponse)) {
+                ?>
+                    <form name='viewer_verify_form' action='php/viewer_verify_user.php' method="post">
+                    <div class="col-sm-4">
+                        <h3><?php echo $row["firstname"] . " " . $row["lastname"] ?></h3>
+                        <table>
+                            <tr>
+                                <th>Age: </th>
+                                <td><?php echo date_diff(date_create($row["birthday"]), date_create('today'))->y ?></td>
+                            </tr>
+                            <tr>
+                                <th>Gender: </th>
+                                <td><?php echo $row["gender"]?></td>
+                            </tr>
+                            <tr>
+                                <th>Phone Number: </th>
+                                <td><?php echo ' ' . $row["phone_no"]?></td>
+                            </tr>
+                            <tr>
+                                <th>Verification Code:</th>
+                                <td><?php echo "<input type='text' name='verification_code'  required>" . " ";?></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                <?php
+                                    echo "<input type='submit' value='Verify'>";
+                                    echo '<input type="hidden" name="user_id" value=' . $row["id"] . '>';
+                                ?>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    </form>
+
+                <?php
+                }
+                ?>
+            </div></div>
+    <?php
+    }
+    else{
+        echo "You are viewing 0 users";
+    }
+
+    ?>
+    <div class = 'container'>
     <br><br>
     <form id="edit_form" action='viewer_profile.php'>
         <input type='submit' value='Edit my profile'>
@@ -33,60 +146,17 @@
     <form id="log_out_form" action='./php/logout.php'>
         <input type='submit' value='Log out'>
     </form>
+    </div>
     <?php
 
-function queryCaregive($viewer_id) {
-    require_once __DIR__.'/php/DB_connect/db_connect.php';
-    $connector = new DB_CONNECT();
-    $connector->connect();
 
-    $caregiveQuery = "SELECT firstname, lastname, birthday, gender, nric, phone_no FROM User, Caregive WHERE rv_id = '$viewer_id' AND user_id = User.id";
-    $caregiveResponse = mysqli_query($connector->conn, $caregiveQuery);
-
-
-    if($caregiveResponse === FALSE) {
-        echo "response is erroneous";
-        die(mysql_error());
-    }
-
-
-
-
-    if(mysqli_num_rows($caregiveResponse) > 0) {
-        echo "<table>
-        <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Gender</th>
-        <th>NRIC</th>
-        <th>Phone Number</th>
-        </tr>";
-
-        while($row = mysqli_fetch_assoc($caregiveResponse)) {
-            echo "<tr>";
-            echo "<td>" . $row["firstname"] . " " . $row["lastname"] . "<br></td>";
-            echo "<td>" . date_diff(date_create($row["birthday"]), date_create('today'))->y . "</td>";
-            echo "<td>" . $row["gender"] . "</td>";
-            echo "<td>" . $row["nric"] . "</td>";
-            echo "<td>" . $row["phone_no"] . "</td>";
-            echo "</tr>";
-
-        }
-    } else {
-        echo "You are viewing 0 users";
-    }
-    echo "</table>";
-    $connector->close();
-
-    return $caregiveResponse;
-}
 
 function queryUnregistered($viewer_phone) {
     require_once __DIR__.'/php/DB_connect/db_connect.php';
     $connector = new DB_CONNECT();
     $connector->connect();
 
-    $unregisteredQuery = "select id, firstname, lastname, U.phone_no, gender, birthday from UnregisteredViewer, User U where '$viewer_phone' = UnregisteredViewer.phone_no AND user_id = U.id";
+
     $unregisteredResponse = mysqli_query($connector->conn, $unregisteredQuery);
 
     if($unregisteredResponse === FALSE) {
