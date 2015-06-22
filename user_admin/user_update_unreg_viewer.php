d292d976f775f5f2d6d31db0ef65d24b9ae59cd1
suheti <suheti.lee@gmail.com>
2015-06-22 14:33:27 +0800

    Bug fix: user_update_unreg_viewer.php directs to wrong directory for logged_out.html; Comment made on index.php

--- user_admin/user_update_unreg_viewer.php
+++ user_admin/user_update_unreg_viewer.php
@@ -5,7 +5,7 @@
 
 session_start ();
 if(!isset($_SESSION ["login_id"]))
-    header("Location: ../logged_out.html");
+    header("Location: logged_out.html");
 
 $user_id = $_SESSION ["login_id"];
 
