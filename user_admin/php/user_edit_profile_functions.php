1102cba2b9b62062406fba6d241f49b13c69704c
Shao Fei <sfatchs@yahoo.com.sg>
2015-06-22 21:58:54 +0800

    Complete partially user edit profile page, left changing of password

---
+++ user_admin/php/user_edit_profile_functions.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shao Fei
+ * Date: 22/6/2015
+ * Time: 4:50 PM
+ */
+
+decideFunction();
+
+function decideFunction() {
+
+    if(isset($_POST['func']) && isset($_POST['params'])) {
+        $function_name = $_POST['func'];
+        $param = $_POST['params'];
+
+        switch($function_name) {
+            case 'loadProfile':
+                return loadProfile();
+            case 'hasEmail':
+                return hasEmail();
+            case 'editDOB':
+                return editDOB($param);
+            case 'editUsername':
+                return editUsername($param);
+            case 'editEmail':
+                return editEmail($param);
+            case 'deleteEmail':
+                return deleteEmail();
+            default:
+                return NULL;
+        }
+    } else {
+        return NULL;
+    }
+}
+
+function loadProfile() {
+    session_start();
+    $result = array();
+    $id = $_SESSION['login_id'];
+
+    require_once __DIR__.'/DB_connect/db_utility.php';
+    $query = "SELECT * FROM User WHERE id='$id'";
+    $response = make_query($query);
+
+    if($response) {
+        if(mysqli_num_rows($response) == 1) {
+            $row = mysqli_fetch_assoc($response);
+            $result['username'] = $row['username'];
+            $result['name'] = $row['firstname'] . " " . $row['lastname'];
+            $result['email'] =  $row['email'];
+            $result['nric'] =  $row['nric'];
+            $result['phone_no'] =  $row['phone_no'];
+            $result['gender'] = $row['gender'];
+            $result['dob'] = $row['birthday'];
+        }
+    }
+
+    echo json_encode($result);
+}
+
+function hasEmail() {
+    session_start();
+    $result = array();
+    $id = $_SESSION['login_id'];
+
+    require_once __DIR__.'/DB_connect/db_utility.php';
+    $query = "SELECT email FROM User WHERE id='$id'";
+    $response = make_query($query);
+    if($response) {
+        $email = mysqli_fetch_assoc($response)['email'];
+        if(!empty($email)) {
+            echo json_encode(true);
+        } else {
+            echo json_encode(false);
+        }
+    } else {
+        echo json_encode(false);
+    }
+}
+
+function editDOB($dob) {
+    require_once __DIR__.'/DB_connect/db_utility.php';
+    session_start();
+    $id = $_SESSION['login_id'];
+    $query = "UPDATE User SET birthday='$dob' WHERE id=$id";
+    $response = make_query($query);
+    if($response) {
+        echo json_encode(true);
+    } else {
+        echo json_encode(false);
+    }
+}
+
+function editUsername($username) {
+    require_once __DIR__.'/DB_connect/db_utility.php';
+    session_start();
+    $id = $_SESSION['login_id'];
+    $query = "UPDATE User SET username='$username' WHERE id=$id";
+    $response = make_query($query);
+    if($response) {
+        echo json_encode(true);
+    } else {
+        echo json_encode(false);
+    }
+}
+
+function editEmail($email) {
+    require_once __DIR__.'/DB_connect/db_utility.php';
+    session_start();
+    $id = $_SESSION['login_id'];
+    $query = "UPDATE User SET email='$email' WHERE id=$id";
+    $response = make_query($query);
+    if($response) {
+        echo json_encode(true);
+    } else {
+        echo json_encode(false);
+    }
+}
+
+function deleteEmail() {
+    require_once __DIR__.'/DB_connect/db_utility.php';
+    session_start();
+    $id = $_SESSION['login_id'];
+    $query = "UPDATE User SET email=NULL WHERE id=$id";
+    $response = make_query($query);
+    if($response) {
+        echo json_encode(true);
+    } else {
+        echo json_encode(false);
+    }
+}
+
+
+?>
---
+++ user_admin/script/user_edit_profile_script.js
@@ -0,0 +1,237 @@
+/**
+ * Created by Shao Fei on 22/6/2015.
+ */
+
+var formValid = {username:false, dob:false, email:false, password:false};
+
+function verifyLogin() {
+    $.post('php/user_add_viewer_functions.php',
+        {
+            func: 'verifyLogin',
+            params: ''
+        },
+        function(data, status) {
+            if(data == true) {
+                window.location.replace("logged_out.html");
+            }
+            console.log(data);
+        });
+}
+
+function loadProfile() {
+    $.post("php/user_edit_profile_functions.php",
+        {
+            func: "loadProfile",
+            params: ""
+
+        },
+        function(data, status) {
+            console.log(data);
+            data = JSON.parse(data);
+            var name = data.name;
+            document.getElementById('user_real_name').innerHTML = name;
+            document.getElementById('name').innerHTML = name;
+            var nric = data.nric;
+            document.getElementById('nric').innerHTML = nric;
+            var gender = data.gender;
+            if(gender==="m" || gender==="M") {
+                gender = "Male";
+            } else if(gender==="f" || gender==="F") {
+                gender = "Female";
+            } else {
+                gender = "";
+            }
+            document.getElementById('gender').innerHTML = gender;
+            var phone_no = data.phone_no;
+            document.getElementById('phone_no').innerHTML = phone_no;
+            var dob = data.dob;
+            document.getElementById('dob').innerHTML = dob;
+            var username = data.username;
+            document.getElementById('username').innerHTML = username;
+            var email = data.email;
+            document.getElementById('email').innerHTML = email;
+        });
+}
+
+function showDeleteEmailButton() {
+    $.post("php/user_edit_profile_functions.php",
+        {
+            func: "hasEmail",
+            params: ""
+
+        },
+        function(data, status) {
+            console.log(data);
+            var hasEmail = JSON.parse(data);
+            if(!hasEmail) {
+                document.getElementById('delete_email_button').style.display = 'none';
+            }
+        });
+}
+
+function validateDOB() {
+    var dobInput = document.getElementById('new_dob').value;
+    var dobDateObject = new Date(dobInput);
+    var todayDateObject = new Date();
+    if (dobInput === "") {
+        document.getElementById('dob_feedback').innerHTML = "";
+        formValid.dob = false;
+    } else if(dobDateObject >= todayDateObject) {
+        document.getElementById('dob_feedback').innerHTML = "Invalid birthday";
+        formValid.dob = false;
+    } else {
+        document.getElementById('dob_feedback').innerHTML = "";
+        formValid.dob = true;
+    }
+}
+
+function editDOB() {
+
+    var dobInput = document.getElementById('new_dob').value;
+
+    validateDOB();
+
+    if(formValid.dob) {
+        $.post("php/user_edit_profile_functions.php",
+            {
+                func: "editDOB",
+                params: dobInput
+
+            },
+            function(data, status) {
+                console.log(data);
+                var success = JSON.parse(data);
+                if(success) {
+                    alert("Date of birth changed");
+                    location.reload();
+                } else {
+                    alert("Error occurred when changing date of birth")
+                }
+            });
+    } else {
+        alert("Invalid date of birth entered");
+    }
+}
+
+function editUsername() {
+    var username = document.getElementById('new_username').value;
+
+    validateUsername();
+
+    if(formValid.username) {
+        $.post("php/user_edit_profile_functions.php",
+            {
+                func: "editUsername",
+                params: username
+
+            },
+            function(data, status) {
+                console.log(data);
+                var success = JSON.parse(data);
+                if(success) {
+                    alert("Username changed");
+                    location.reload();
+                } else {
+                    alert("Error occurred when changing username")
+                }
+            });
+    } else {
+        alert("Invalid username entered");
+    }
+}
+
+function editEmail() {
+    var email = document.getElementById('new_email').value;
+
+    validateEmail();
+
+    if(formValid.email) {
+        $.post("php/user_edit_profile_functions.php",
+            {
+                func: "editEmail",
+                params: email
+
+            },
+            function(data, status) {
+                console.log(data);
+                var success = JSON.parse(data);
+                if(success) {
+                    alert("Email changed");
+                    location.reload();
+                } else {
+                    alert("Error occurred when changing email")
+                }
+            });
+    } else {
+        alert("Invalid email entered");
+    }
+}
+
+function deleteEmail() {
+
+    $.post("php/user_edit_profile_functions.php",
+        {
+            func: "deleteEmail",
+            params: ""
+
+        },
+        function(data, status) {
+            console.log(data);
+            var success = JSON.parse(data);
+            if(success) {
+                alert("Email deleted");
+                location.reload();
+            } else {
+                alert("Error occurred when deleting email")
+            }
+        });
+}
+
+function validateUsername() {
+    var username = document.getElementById('new_username').value;
+    if(username === "") {
+        document.getElementById("username_feedback").innerHTML = "";
+        formValid.username=false;
+    } else if(/[\s]/.test(username)) {
+        document.getElementById("username_feedback").innerHTML = "Username should not contain blank space";
+        formValid.username=false;
+    } else if(username.length < 4) {
+        document.getElementById("username_feedback").innerHTML = "Username below 4 characters";
+        formValid.username=false;
+    }  else {
+        checkUsernameExists(username);
+    }
+}
+
+function checkUsernameExists(username) {
+    $.get("php/check_username_exists.php",
+        {
+            username: username
+        },
+        function(data) {
+            console.log(data);
+            if(data === '1') {
+                document.getElementById("username_feedback").innerHTML = "Username already exists";
+                formValid.username = false;
+            } else {
+                document.getElementById("username_feedback").innerHTML = "";
+                formValid.username = true;
+            }
+        });
+}
+
+function validateEmail() {
+    var emailField = document.getElementById("new_email");
+    if(!emailField.checkValidity()) {
+        document.getElementById("email_feedback").innerHTML = "Invalid email";
+        formValid.email = false;
+    } else {
+        document.getElementById("email_feedback").innerHTML = "";
+        formValid.email = true;
+    }
+}
+
+
+
+
+
